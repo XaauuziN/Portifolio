c979d4ffe536ff64fca2d65ad45c5a13a6740fcc
Gabriel Felicio <137421493+XaauuziN@users.noreply.github.com>
2025-02-24 20:22:20 -0300

    JS CONFIG

--- Login/Login.js
+++ Login/Login.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         // Redirecionar para a página de home (ou enviar dados para o servidor)
-        window.location.href = '../Home/Home.html';
+        window.location.href = 'Home/Home.html';
     });
 
 
